52faecb06d7640b347e0106a7372481ab903ff6c
Adrian <72496013+adrianmrt@users.noreply.github.com>
2021-10-13 13:10:13 +0200

    CameraMov Proto

--- Assets/Scripts/Camera/UICameraMovement.cs
+++ Assets/Scripts/Camera/UICameraMovement.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class UICameraMovement : MonoBehaviour
 {
-    Vector3 _position;
-    Vector3 _actRot;
-    int[] rotations;
-    int side;// side in which the point is pointing to at the begining. 0 for top, 1 for bottom
+    public GameObject[] cameraPositions; //1-4 for top and lateral faces, 5-8 for bot and lateral faces
+    int currentCamPos;
+
+    enum face {top,bot}
+    face currentFace; //if we can see top or bot face.
 
     // Start is called before the first frame update
     void Start()
     {
-        _position = gameObject.transform.position;
-        _actRot = gameObject.transform.rotation.eulerAngles;
+        currentCamPos = 0;
+        currentFace = face.top;
+        setCameraOrientation(currentCamPos);
     }
 
     // Update is called once per frame
@@ -25,23 +27,82 @@ public class UICameraMovement : MonoBehaviour
 
     public void rotateToRight()
     {
-        if (_actRot.z != 125)
+        if (currentFace == face.top)
         {
-            gameObject.transform.position = gameObject.transform.position + new Vector3(-3, -2, 0);
-            gameObject.transform.Rotate(0, 0, 125);
-            _actRot = gameObject.transform.rotation.eulerAngles;
+            if (currentCamPos < 3)
+            {
+                currentCamPos += 1;
+            }
+            else
+            {
+                currentCamPos = 0;
+            }
         }
-        Debug.Log(_actRot);
+        else
+        {
+            if (currentCamPos <7)
+            {
+                currentCamPos += 1;
+            }
+            else
+            {
+                currentCamPos = 4;
+            }
+        }
+        setCameraOrientation(currentCamPos);
     }
 
     public void rotateToLeft()
     {
-        if (_actRot.z != 235)
+        if (currentFace == face.top)
         {
-            gameObject.transform.position = gameObject.transform.position + new Vector3(3, 2, 0);
-            gameObject.transform.Rotate(0, 0, -125);
-            _actRot = gameObject.transform.rotation.eulerAngles;
+            if (currentCamPos > 0)
+            {
+                currentCamPos -= 1;
+            }
+            else
+            {
+                currentCamPos = 3;
+            }
         }
-        Debug.Log(_actRot);
+        else
+        {
+            if (currentCamPos > 4)
+            {
+                currentCamPos -= 1;
+            }
+            else
+            {
+                currentCamPos = 7;
+            }
+        }
+        setCameraOrientation(currentCamPos);
     }
+
+    public void rotateToUp()
+    {
+        if (currentFace == face.bot)
+        {
+            currentFace = face.top;
+            currentCamPos -= 4;
+            setCameraOrientation(currentCamPos);
+        }
+    }
+
+    public void rotateToBot()
+    {
+        if (currentFace == face.top)
+        {
+            currentFace = face.bot;
+            currentCamPos += 4;
+            setCameraOrientation(currentCamPos);
+        }
+    }
+
+    void setCameraOrientation(int pos)
+    {
+        gameObject.transform.position = cameraPositions[pos].transform.position;
+        gameObject.transform.rotation = cameraPositions[pos].transform.rotation;
+    }
+
 }
