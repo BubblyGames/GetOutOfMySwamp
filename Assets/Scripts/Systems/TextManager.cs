34fd6e9f9c0efb9734e7403c7ae553e7666c5d3d
Mr.T <72496013+adrianmrt@users.noreply.github.com>
2021-11-16 15:14:29 +0100

    Dictionary Initialization

--- Assets/Scripts/Systems/TextManager.cs
+++ Assets/Scripts/Systems/TextManager.cs
@@ -11,6 +11,22 @@ public class TextManager: MonoBehaviour
     public Dictionary<string, string> spanishDictionary = new Dictionary<string, string>();
     public Dictionary<string, string> currentDictionary;
 
+    /*Type of keys 
+        Play,
+        Pause,
+        Score,
+        Money,
+        Settings,
+        Exit,
+        Lesson,
+        Description
+    */
+
+    private void Awake()
+    {
+        InitializeDictionaries();
+    }
+
     private void Start()
     {
         currentDictionary = englishDictionary;
@@ -42,6 +58,31 @@ public class TextManager: MonoBehaviour
             currentDictionary = spanishDictionary;
         }
         UpdateSubscribers();
+    }
+
+    private void InitializeDictionaries()
+    {
+        //English 
+        englishDictionary.Add("play", "Play");
+        englishDictionary.Add("pause", "Pause");
+        englishDictionary.Add("score", "Score");
+        englishDictionary.Add("money", "Money");
+        englishDictionary.Add("settings", "Settings");
+        englishDictionary.Add("exit", "Exit");
+        englishDictionary.Add("lesson", "Lesson");
+        //descriptions of lessons in English
+        englishDictionary.Add("description", "asadsafdsfdsfdsfsdfds");
+
+        //Spanish 
+        spanishDictionary.Add("play", "Jugar");
+        spanishDictionary.Add("pause", "Pausa");
+        spanishDictionary.Add("score", "Puntuación");
+        spanishDictionary.Add("money", "Dinero");
+        spanishDictionary.Add("settings", "Ajustes");
+        spanishDictionary.Add("exit", "Sair");
+        spanishDictionary.Add("lesson", "Lección");
+        //descriptions of lessons in Spanish
+        spanishDictionary.Add("description", "asadsafdsfdsfdsfsdfds");
     }
 
 }
