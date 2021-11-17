a0558f55f676c2526ca3aca7c9b93ef84c08f231
Adrian <72496013+adrianmrt@users.noreply.github.com>
2021-11-17 13:03:38 +0100

    fixed bugs related to change lenguage

--- Assets/Scripts/Systems/TextReader.cs
+++ Assets/Scripts/Systems/TextReader.cs
@@ -60,15 +60,15 @@ public class TextReader : MonoBehaviour
 
         if (gameObject.name.Equals("hpText"))
         {
-            textContainer.GetComponent<Text>().text = _text; ;
+            textContainer.GetComponent<Text>().text = levelManager.GetComponent<LevelStats>().GetCurrentBaseHealth().ToString();
         }
         else if (gameObject.name.Equals("moneyText"))
         {
-            textContainer.GetComponent<Text>().text = _text; ;
+            textContainer.GetComponent<Text>().text = levelManager.GetComponent<LevelStats>().GetCurrentMoney().ToString();
         }
         else if (gameObject.name.Equals("scoreText"))
         {
-            textContainer.GetComponent<Text>().text = _text; ;
+            textContainer.GetComponent<Text>().text = _text;
         }
         else
         {
