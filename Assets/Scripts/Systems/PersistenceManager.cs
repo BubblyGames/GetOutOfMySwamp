08b5cdcc608c4ea804a00f51256be01cd00139a9
Mr.T <72496013+adrianmrt@users.noreply.github.com>
2021-12-07 12:48:31 +0100

    Persistence Score and Tutorial

--- Assets/Scripts/Systems/PersistenceManager.cs
+++ Assets/Scripts/Systems/PersistenceManager.cs
@@ -8,7 +8,7 @@ public class PersistenceManager
     {
         GameManager gameManager = GameManager.instance;
         BinaryFormatter formatter = new BinaryFormatter();
-        string path = Application.persistentDataPath+"/player.txt";
+        string path = Application.persistentDataPath + "/player.txt";
         FileStream stream = new FileStream(path, FileMode.Create);
         PlayerData data = new PlayerData(gameManager);
 
@@ -18,7 +18,7 @@ public class PersistenceManager
 
     public static PlayerData LoadData()
     {
-        string path = Application.persistentDataPath+"/player.txt";
+        string path = Application.persistentDataPath + "/player.txt";
         if (File.Exists(path))
         {
             BinaryFormatter formatter = new BinaryFormatter();
@@ -33,6 +33,17 @@ public class PersistenceManager
             Debug.Log("Save file not found");
             return null;
         }
-      
+    }
+
+    public static void SaveScoreData(PlayerData playerData, int index, int score)
+    {
+        playerData.SaveScore(index, score);
+        SaveData(playerData);
+    }
+
+    public static void SaveDefenseUsedData(PlayerData playerData, int index)
+    {
+        playerData.DefenseUsed(index);
+        SaveData(playerData);
     }
 }
