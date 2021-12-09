e5b8ab627f8425d825333d704de7e5f29fa6d60f
Marta Vidal <57014218+MartaVidal@users.noreply.github.com>
2021-12-09 14:44:00 +0100

    Loading Animation

--- Assets/Scripts/Systems/LoadingAnimation.cs
+++ Assets/Scripts/Systems/LoadingAnimation.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class LoadingAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Start is called before t
+
+    private void Update()
     {
-        
+        if (gameObject.activeSelf) {
+            transform.rotation = Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z + Time.deltaTime * 50f);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
